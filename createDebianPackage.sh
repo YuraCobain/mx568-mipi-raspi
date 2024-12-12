3c8425009246af03f26a540cd308ada4b4f80d8e
Florian Schmid <florian.schmid@vision-components.com>
2024-12-12 11:32:56 +0000

    Update github actions for tag name

--- createDebianPackage.sh
+++ createDebianPackage.sh
@@ -2,12 +2,25 @@ sudo rm -rf build
 
 mkdir -p build
 
+# Set version if not set
+if [ -z "$VERSION" ]; then
+    export VERSION="0.0.0"
+fi
+
+
 
 cp -r debian_package build/debian
 cp -r vc-mipi-driver-bcm2712 build/debian
 mkdir -p build/debian/tmp
 cp -r src build/
-cp dkms.conf build/
+envsubst < debian_package/changelog > build/debian/changelog
+envsubst < dkms.conf > build/dkms.conf
+envsubst < debian_package/control > build/debian/control
+envsubst < debian_package/not-installed > build/debian/not-installed
+envsubst < debian_package/postinst > build/debian/postinst
+envsubst < debian_package/postrm > build/debian/postrm
+envsubst < debian_package/rules > build/debian/rules
+envsubst < debian_package/vc-mipi-driver-bcm2712.install > build/debian/vc-mipi-driver-bcm2712.install 
 
 cd build
 sudo dpkg-buildpackage -us -uc -b 
